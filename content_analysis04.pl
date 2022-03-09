b28434393b108fb72aff80ca02afff21df9d65a0
13c13rq <leclerq@riseup.net>
2022-03-09 22:44:36 +0100

    smol changes

--- content_analysis04.pl
+++ content_analysis04.pl
@@ -933,19 +933,19 @@ sub assesment {
 					};
 					
 		#	#	#	#source filter
-					#my $escalated_temp	= $findings[$currnent_grouping][$temp_grouping_scalar] {"escalated_source"};
-					#my $unique_temp		= $findings[$currnent_grouping][$temp_grouping_scalar] {"unique_source"};
+					my $escalated_temp	= $findings[$currnent_grouping][$temp_grouping_scalar] {"escalated_source"};
+					my $unique_temp		= $findings[$currnent_grouping][$temp_grouping_scalar] {"unique_source"};
 	
-					#if (defined($escalated_temp) or $unique_temp ne 'null'){
-						#my @relevant_words_temp;
-						#if (defined($escalated_temp)) {push (@relevant_words_temp, "$escalated_temp -> ++ ");
-							#$loop_sum++;
-						#};
-						#if ($unique_temp ne 'null')  {push (@relevant_words_temp, "$unique_temp -> +++");	
-							#$loop_sum++;
-						#};
-						#print "relevant source: ( @relevant_words_temp", "), ";
-						#print $Hf1 "relevant source: ( @relevant_words_temp", "), ";
+					if (defined($escalated_temp) or $unique_temp ne 'null'){
+						my @relevant_words_temp;
+						if (defined($escalated_temp)) {push (@relevant_words_temp, "$escalated_temp -> ++ ");
+							$loop_sum++;
+						};
+						if ($unique_temp ne 'null')  {push (@relevant_words_temp, "$unique_temp -> +++");	
+							$loop_sum++;
+						};
+						print "relevant source: ( @relevant_words_temp", "), ";
+						print $Hf1 "relevant source: ( @relevant_words_temp", "), ";
 					#};
 					
 		#	#	#	#time filter
